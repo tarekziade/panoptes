19c43966768ed005a220a4fa6fc62d775f14bf46
Tarek Ziade <tarek@ziade.org>
2018-11-21 18:15:25 +0100

    black formatting

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from setuptools import setup, find_packages
 
 
 install_requires = []
-description = ''
+description = ""
 
-setup(name='panoptes',
-      version='0.1.',
-      packages=find_packages(),
-      author="Tarek Ziade",
-      author_email="tarek@ziade.org",
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=install_requires)
-
+setup(
+    name="panoptes",
+    version="0.1.",
+    packages=find_packages(),
+    author="Tarek Ziade",
+    author_email="tarek@ziade.org",
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+)
